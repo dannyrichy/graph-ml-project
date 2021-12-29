f5e80f8d7c6c80d5ad7438e512f55408e425d3d8
Daniel Richards R <richydany@gmail.com>
2021-12-29 17:22:30 +0100

    Proj structure (#2)
    * utils.py added with negative sampling
    
    * Some more  structure

---
+++ main.py
@@ -0,0 +1,8 @@
+import networkx as nx
+
+from utils import read_data
+
+if __name__ == '__main__':
+    edge_list = read_data("../graph-ml-project/data/out.munmun_twitter_social")
+    graph = nx.DiGraph()
+    graph.add_weighted_edges_from(edge_list)
---
+++ utils.py
@@ -0,0 +1,90 @@
+"""
+Common utility function
+"""
+import math
+from operator import methodcaller
+
+import numpy as np
+
+
+def read_data(filename, header=2):
+    with open(filename, 'r') as f:
+        list_edges = list(map(lambda x: (x[0], x[1], 1), list(map(methodcaller("split", " "), f.read().splitlines()[header:]))))
+    return list_edges
+
+
+def create_alias_table(area_ratio):
+    """
+
+    :param area_ratio: sum(area_ratio)=1
+    :return: accept,alias
+    """
+    l = len(area_ratio)
+    accept, alias = [0] * l, [0] * l
+    small, large = [], []
+    area_ratio_ = np.array(area_ratio) * l
+    for i, prob in enumerate(area_ratio_):
+        if prob < 1.0:
+            small.append(i)
+        else:
+            large.append(i)
+
+    while small and large:
+        small_idx, large_idx = small.pop(), large.pop()
+        accept[small_idx] = area_ratio_[small_idx]
+        alias[small_idx] = large_idx
+        area_ratio_[large_idx] = area_ratio_[large_idx] - \
+                                 (1 - area_ratio_[small_idx])
+        if area_ratio_[large_idx] < 1.0:
+            small.append(large_idx)
+        else:
+            large.append(large_idx)
+
+    while large:
+        large_idx = large.pop()
+        accept[large_idx] = 1
+    while small:
+        small_idx = small.pop()
+        accept[small_idx] = 1
+
+    return accept, alias
+
+
+def alias_sampling(graph):
+    """
+
+    :param graph:
+    :type graph: networkx.Graph
+    :return:
+    :rtype:
+    """
+    power = 0.75
+    no_nodes = graph.number_of_nodes()
+    no_edges = graph.number_of_edges()
+    node2idx = {}
+    idx2node = []
+    node_size = 0
+    for node in graph.nodes():
+        node2idx[node] = node_size
+        idx2node.append(node)
+        node_size += 1
+    node_degree = np.zeros(no_nodes)  # out degree
+
+    for edge in graph.edges():
+        node_degree[node2idx[edge[0]]] += graph[edge[0]][edge[1]].get('weight', 1.0)
+
+    norm_prob = [float(math.pow(node_degree[j], power)) /
+                 sum([math.pow(node_degree[i], power) for i in range(no_nodes)])
+                 for j in range(no_nodes)]
+
+    node_accept, node_alias = create_alias_table(norm_prob)
+
+    # create sampling table for edge
+
+    norm_prob = [graph[edge[0]][edge[1]].get('weight', 1.0) *
+                 no_edges / sum([graph[edge[0]][edge[1]].get('weight', 1.0)
+                                 for edge in graph.edges()])
+                 for edge in graph.edges()]
+
+    edge_accept, edge_alias = create_alias_table(norm_prob)
+    return node_accept, node_alias, edge_accept, edge_alias, node2idx
