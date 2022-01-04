b8362edb72ef27b208eefbe87a9ad426a534db50
Daniel Richards R <richydany@gmail.com>
2022-01-04 18:36:36 +0100

    Alias sampling (#4)
    * Base model created
    
    * First draft of model
    
    * Working graph model
    
    * Train test implemented
    
    * Metrics added to the model
    
    * I think I know, but do I know
    
    * Finally loss is reducing

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
+import logging
+import random
+
 import networkx as nx
 
+from line.tasks import LinkPredict
 from utils import read_data
 
-if __name__ == '__main__':
-    edge_list = read_data("../graph-ml-project/data/out.munmun_twitter_social")
+logging.basicConfig(
+    format='%(process)d-%(levelname)s-%(message)s',
+    level=logging.INFO)
+
+
+def prepare_train_test(graph):
+    """
+    Train test split
+    :param graph:
+    :type graph: networkx.Graph
+    :return:
+    :rtype:
+    """
+    return random.sample([(u, v, weight['weight']) for u, v, weight in graph.edges(data=True)], int(0.25 * graph.number_of_edges()))
+
+
+def main(file_loc="../graph-ml-project/data/out.munmun_twitter_social", n_iter=20, batch_size=1024):
+    logging.info("Reading the graph data")
+    edge_list = read_data(file_loc)
     graph = nx.DiGraph()
+
+    logging.info("Adding the edges")
     graph.add_weighted_edges_from(edge_list)
+
+    logging.info("Preparing train test split")
+    test_set = prepare_train_test(graph=graph)
+    test_graph = nx.DiGraph()
+    test_graph.add_weighted_edges_from(test_set)
+    train_graph = graph.copy()
+    train_graph.remove_edges_from(test_set)
+    del graph
+    logging.info("Constructed the graph")
+
+    l = LinkPredict(train_graph=train_graph, batch_size=batch_size)
+    l.run(epochs=n_iter)
+    l.evaluate(test_graph)
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 """
-Common utility function
+Common utility functions
 """
-import math
 from operator import methodcaller
 
 import numpy as np
@@ -13,78 +12,60 @@ def read_data(filename, header=2):
     return list_edges
 
 
-def create_alias_table(area_ratio):
-    """
+class AliasTable:
+    def __init__(self, prob_dist):
+        """
+        Class to generate the alias table
 
-    :param area_ratio: sum(area_ratio)=1
-    :return: accept,alias
-    """
-    l = len(area_ratio)
-    accept, alias = [0] * l, [0] * l
-    small, large = [], []
-    area_ratio_ = np.array(area_ratio) * l
-    for i, prob in enumerate(area_ratio_):
-        if prob < 1.0:
-            small.append(i)
-        else:
-            large.append(i)
+        :param prob_dist: Probability distribution to use
+        :type prob_dist: list
 
-    while small and large:
-        small_idx, large_idx = small.pop(), large.pop()
-        accept[small_idx] = area_ratio_[small_idx]
-        alias[small_idx] = large_idx
-        area_ratio_[large_idx] = area_ratio_[large_idx] - \
-                                 (1 - area_ratio_[small_idx])
-        if area_ratio_[large_idx] < 1.0:
-            small.append(large_idx)
-        else:
-            large.append(large_idx)
+        :return: None
+        :rtype: Nothing
+        """
+        self.prob = prob_dist
+        self.num_pts = len(self.prob)
+        self.accept = np.zeros(self.num_pts)
+        self.alias = np.zeros(self.num_pts)
+        self.create_alias_table()
 
-    while large:
-        large_idx = large.pop()
-        accept[large_idx] = 1
-    while small:
-        small_idx = small.pop()
-        accept[small_idx] = 1
+    def create_alias_table(self):
+        """
+        Generates the alias and accept list
+        :return: Nothing
+        :rtype: None
+        """
+        small, large = list(), list()
+        area_ratio_ = np.array(self.prob) * self.num_pts
+        for i, prob in enumerate(area_ratio_):
+            if prob < 1.0:
+                small.append(i)
+            else:
+                large.append(i)
 
-    return accept, alias
+        while small and large:
+            small_idx, large_idx = small.pop(), large.pop()
+            self.accept[small_idx] = area_ratio_[small_idx]
+            self.alias[small_idx] = large_idx
+            area_ratio_[large_idx] = area_ratio_[large_idx] - (1 - area_ratio_[small_idx])
+            if area_ratio_[large_idx] < 1.0:
+                small.append(large_idx)
+            else:
+                large.append(large_idx)
 
+        while large:
+            large_idx = large.pop()
+            self.accept[large_idx] = 1
+        while small:
+            small_idx = small.pop()
+            self.accept[small_idx] = 1
 
-def alias_sampling(graph):
-    """
+    def alias_sample(self):
+        """
+        Sample from the generated list
 
-    :param graph:
-    :type graph: networkx.Graph
-    :return:
-    :rtype:
-    """
-    power = 0.75
-    no_nodes = graph.number_of_nodes()
-    no_edges = graph.number_of_edges()
-    node2idx = {}
-    idx2node = []
-    node_size = 0
-    for node in graph.nodes():
-        node2idx[node] = node_size
-        idx2node.append(node)
-        node_size += 1
-    node_degree = np.zeros(no_nodes)  # out degree
-
-    for edge in graph.edges():
-        node_degree[node2idx[edge[0]]] += graph[edge[0]][edge[1]].get('weight', 1.0)
-
-    norm_prob = [float(math.pow(node_degree[j], power)) /
-                 sum([math.pow(node_degree[i], power) for i in range(no_nodes)])
-                 for j in range(no_nodes)]
-
-    node_accept, node_alias = create_alias_table(norm_prob)
-
-    # create sampling table for edge
-
-    norm_prob = [graph[edge[0]][edge[1]].get('weight', 1.0) *
-                 no_edges / sum([graph[edge[0]][edge[1]].get('weight', 1.0)
-                                 for edge in graph.edges()])
-                 for edge in graph.edges()]
-
-    edge_accept, edge_alias = create_alias_table(norm_prob)
-    return node_accept, node_alias, edge_accept, edge_alias, node2idx
+        :return: index
+        :rtype: int
+        """
+        i = int(np.random.random() * self.num_pts)
+        return i if np.random.random() < self.accept[i] else self.alias[i]
