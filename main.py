d3d95b3eecbe31e2f7f2df32081fd9c9aba04da8
richydany@gmail.com <richydany@gmail.com>
2022-01-05 18:08:02 +0100

    Added BlogCatLog datasets

--- main.py
+++ main.py
@@ -55,20 +55,20 @@ def netmf(train_graph, test_graph, b, T, win_size="small"):
 def main(file_loc="../graph-ml-project/data/out.munmun_twitter_social"):
     # Twitter data
     logging.info("Reading the graph data")
-    edge_list = read_twitter_edges(file_loc)
-    graph = nx.DiGraph()
-
-    logging.info("Adding the edges")
-    graph.add_weighted_edges_from(edge_list)
-
-    logging.info("Preparing train test split")
-    test_set = prepare_train_test(graph=graph)
-    test_graph = nx.DiGraph()
-    test_graph.add_weighted_edges_from(test_set)
-    train_graph = graph.copy()
-    train_graph.remove_edges_from(test_set)
-    del graph
-    logging.info("Constructed the graph")
+    # edge_list = read_twitter_edges(file_loc)
+    # graph = nx.DiGraph()
+    #
+    # logging.info("Adding the edges")
+    # graph.add_weighted_edges_from(edge_list)
+    #
+    # logging.info("Preparing train test split")
+    # test_set = prepare_train_test(graph=graph)
+    # test_graph = nx.DiGraph()
+    # test_graph.add_weighted_edges_from(test_set)
+    # train_graph = graph.copy()
+    # train_graph.remove_edges_from(test_set)
+    # del graph
+    # logging.info("Constructed the graph")
 
     # BlogCatalog
     edge_list = read_blog_catalog_edges("/content/soc-BlogCatalog-ASU.edges")
